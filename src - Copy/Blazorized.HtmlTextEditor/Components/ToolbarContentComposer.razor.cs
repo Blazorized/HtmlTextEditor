174fdcdbe2e7251b279d007e0b29e4b15b62e053
Mark Galvin <mark@thegalvin.com>
2022-12-17 21:13:49 -0500

    Clean-up

--- src - Copy/Blazorized.HtmlTextEditor/Components/ToolbarContentComposer.razor.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components;
-namespace Blazorized.HtmlTextEditor;
-
-public partial class ToolbarContentComposer
-{
-    [CascadingParameter] public Toolbar Toolbar { get; set; } = default!;
-    [CascadingParameter] public List<string> Fonts { get; set; } = new();
-}
